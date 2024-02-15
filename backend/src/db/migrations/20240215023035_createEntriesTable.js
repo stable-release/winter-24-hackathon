33835ffd244d36bd2c60270393145f1b091232d4
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2024-02-14 22:27:20 -0500

    initial commit

---
+++ backend/src/db/migrations/20240215023035_createEntriesTable.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("entries", (table) => {
+    table.increments("entry_id").primary();
+    table.integer("user_id").unsigned().notNullable();
+    table
+        .foreign("user_id")
+        .references("user_id")
+        .inTable("users")
+        .onDelete("cascade");
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("entries");
+};
