871e43dd7d75d54cddaf9f4bf9583b781fd16e63
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2024-02-15 21:50:06 -0500

    removed route for PUT request on employees.router file

--- backend/src/employees/employees.router.js
+++ backend/src/employees/employees.router.js
@@ -5,7 +5,6 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 router.route("/").get(controller.list).all(methodNotAllowed);
 router.route("/new").post(controller.create).all(methodNotAllowed);
 router.route("/:user_id")
-//     .put(controller.update)
     .delete(controller.delete)
     .all(methodNotAllowed);
 
