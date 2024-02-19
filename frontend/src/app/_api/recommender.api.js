8bfc0ada5f7a20e06d97a537738b37c84d14be32
stable-release <stable-release@users.noreply.github.com>
2024-02-19 02:46:31 -0600

    Frontend API for recommender

--- frontend/src/app/_api/recommender.api.js
+++ frontend/src/app/_api/recommender.api.js
@@ -2,37 +2,30 @@ require("dotenv").config();
 
 const { API_URL = "http://localhost:5001" } = process.env;
 
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
-
-async function fetchJson(url, options, onCancel) {
+/**
+ * Recommends a strategy for specified user
+ */
+export async function recommendStrategy(username) {
     try {
-        const response = await fetch(url, options);
-
-        if (response.status === 204) {
-            return null;
+        // fetch verification
+        // return { username: "", permissions: 1 };
+        let headersList = {
+            Accept: "*/*",
+            "Content-Type": "application/json",
         };
 
-        const payload = await response.json();
+        let response = await fetch(`${API_URL}/recommender/${username}`, {
+            method: "GET",
+            headers: headersList,
+        });
+
+        let payload = await response.json();
 
         if (payload.error) {
             return Promise.reject({ message: payload.error });
-        };
-
+        }
         return payload.data;
-
-    } catch (error) {
-        if (error.name !== "AbortError") {
-            console.error(error.stack);
-            throw error;
-        };
-
-        return Promise.resolve(onCancel);
-    };
-};
-
-export async function listEmployees(username, signal) {
-    const url = new URL(`${API_URL}/recommender/${username}`);
-    const options = { method: "GET", signal};
-    return await fetchJson(url, options, []);
+    } catch (e) {
+        console.error(e);
+    }
 }
