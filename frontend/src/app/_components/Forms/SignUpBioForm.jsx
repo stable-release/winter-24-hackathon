d366325508b6f56b1c49eab186b1300f18089cde
stable-release <stable-release@users.noreply.github.com>
2024-02-19 21:22:46 -0600

    Sign Up Bio form

--- frontend/src/app/_components/Forms/SignUpBioForm.jsx
+++ frontend/src/app/_components/Forms/SignUpBioForm.jsx
@@ -5,80 +5,151 @@ export default function SignUpBioForm({
     error,
 }) {
     return (
-        <form onSubmit={onSubmit} className="flex flex-col">
-            <input
-                id="birthdate"
-                name="birthdate"
-                type="text"
-                autoComplete="birthdate"
-                required={true}
-                placeholder="birthdate"
-                onChange={handleChange}
-                value={formData.birthdate}
-            />
-            <input
-                id="height"
-                name="height"
-                type="height"
-                autoComplete="height"
-                required={true}
-                placeholder="height"
-                onChange={handleChange}
-                value={formData.height}
-            />
-            <input
-                id="weight"
-                name="weight"
-                type="number"
-                autoComplete="weight"
-                required={true}
-                placeholder="weight"
-                onChange={handleChange}
-                value={formData.weight}
-            />
-            <select name="sex" id="sex">
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="none" selected={true}>
-                    none
-                </option>
-            </select>
-            <input
-                id="occupation"
-                name="occupation"
-                type="occupation"
-                autoComplete="occupation"
-                required={true}
-                onChange={handleChange}
-                value={formData.occupation}
-            />
-            <input
-                id="income"
-                name="income"
-                type="number"
-                autoComplete="income"
-                required={true}
-                onChange={handleChange}
-                value={formData.income}
-            />
-            <input
-                id="location"
-                name="location"
-                type="location"
-                autoComplete="none"
-                required={true}
-                onChange={handleChange}
-                value={formData.location}
-            />
-            <select name="sleeping_disorder" id="sleeping_disorder">
-                <option value="apnea">Apnea</option>
-                <option value="insomnia">Insomnia</option>
-                <option value="none" selected={true}>
-                    none
-                </option>
-            </select>
+        <form onSubmit={onSubmit} className="flex flex-col gap-[40px]">
+            <div className="flex flex-col gap-3">
+                <label htmlFor="birthdate" className="FormLabel">
+                    Birthdate
+                </label>
+                <input
+                    id="birthdate"
+                    name="birthdate"
+                    type="text"
+                    autoComplete="birthdate"
+                    required={true}
+                    placeholder="Birthdate"
+                    onChange={handleChange}
+                    value={formData.birthdate}
+                    className="FormInput"
+                />
+            </div>
+            <div className="flex gap-[50px]">
+                <div className="w-1/4">
+                    <div className="flex flex-col gap-3">
+                        <label htmlFor="height" className="FormLabel">
+                            Height
+                        </label>
+                        <input
+                            id="height"
+                            name="height"
+                            type="height"
+                            autoComplete="height"
+                            required={true}
+                            placeholder="height"
+                            onChange={handleChange}
+                            value={formData.height}
+                            className="FormInput"
+                        />
+                    </div>
+                </div>
+                <div className="w-1/4">
+                    <div className="flex flex-col gap-3">
+                        <label htmlFor="weight" className="FormLabel">
+                            Weight
+                        </label>
+                        <input
+                            id="weight"
+                            name="weight"
+                            type="number"
+                            autoComplete="weight"
+                            required={true}
+                            placeholder="weight"
+                            onChange={handleChange}
+                            value={formData.weight}
+                            className="FormInput"
+                        />
+                    </div>
+                </div>
+                <div className="w-1/4">
+                    <div className="flex flex-col gap-3">
+                        <label htmlFor="sex" className="FormLabel">
+                            Sex
+                        </label>
+                        <select name="sex" id="sex" className="FormInput">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="none" selected={true}>
+                                none
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <label htmlFor="occupation" className="FormLabel">
+                    Occupation
+                </label>
+                <input
+                    id="occupation"
+                    name="occupation"
+                    type="occupation"
+                    autoComplete="occupation"
+                    required={true}
+                    onChange={handleChange}
+                    value={formData.occupation}
+                    className="FormInput"
+                />
+            </div>
+            <div className="flex gap-[50px]">
+                <div className="w-1/4">
+                    <div className="flex flex-col gap-3">
+                        <label htmlFor="income" className="FormLabel">
+                            Income
+                        </label>
+                        <input
+                            id="income"
+                            name="income"
+                            type="number"
+                            autoComplete="income"
+                            required={true}
+                            onChange={handleChange}
+                            value={formData.income}
+                            className="FormInput"
+                        />
+                    </div>
+                </div>
+                <div className="w-1/4">
+                    <div className="flex flex-col gap-3">
+                        <label htmlFor="location" className="FormLabel">
+                            Location
+                        </label>
+                        <input
+                            id="location"
+                            name="location"
+                            type="location"
+                            autoComplete="none"
+                            required={true}
+                            onChange={handleChange}
+                            value={formData.location}
+                            className="FormInput"
+                        />
+                    </div>
+                </div>
+                <div className="w-1/4">
+                    <div className="flex flex-col gap-3">
+                        <label
+                            htmlFor="sleeping_disorder"
+                            className="FormLabel"
+                        >
+                            Sleeping Disorder
+                        </label>
+                        <select
+                            name="sleeping_disorder"
+                            id="sleeping_disorder"
+                            className="FormInput"
+                        >
+                            <option value="apnea">Apnea</option>
+                            <option value="insomnia">Insomnia</option>
+                            <option value="none" selected={true}>
+                                none
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
             {error && <div>{error}</div>}
-            <button type="submit">Finish Sign Up</button>
+            <button type="submit" className="FormSubmit">
+                Finish Sign Up
+            </button>
         </form>
     );
 }
