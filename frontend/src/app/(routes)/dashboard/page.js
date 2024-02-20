83059264b42cdcdf92bff66af6106b4d43459667
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2024-02-19 22:20:34 -0500

    added calendar component that can be used anywhere in the app

--- frontend/src/app/(routes)/dashboard/page.js
+++ frontend/src/app/(routes)/dashboard/page.js
@@ -3,10 +3,13 @@
 import { getCookie } from "cookies-next";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function Page() {
     // Auth with cookies 🍪
     const [perm, setPerm] = useState(0);
+    const [value, onChange] = useState(new Date());
 
     useEffect(() => {
       const permission = getCookie("permissions");
@@ -17,6 +20,7 @@ export default function Page() {
       <>
         <div>{perm > 0 && <h1>Hello, Dashboard</h1>}</div>
         <div>{perm == 2 ? <Link href="/employees">View Employees</Link> : null}</div>
+        <Calendar onChange={onChange} value={value} />
       </>
     );
 }
